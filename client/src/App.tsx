fcc4fd1d6d7a91faf1a202b4f495076a10dc8d9e
arion <mearion@gmail.com>
2019-09-04 11:48:10 +0400

    Add some base styles, navigation menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom"
+
+import cn from 'classnames'
 
 import * as Store from './context/store'
 
@@ -10,12 +12,18 @@ import LandingPage from './pages/landing'
 import UsersPage from './pages/users'
 
 const App: React.FC = () => {
+  const [navToggle, setNavToggle] = useState(false)
+
   return (
     <Router>
       <Store.Context>
-        <Navigation></Navigation>
-        <Route path="/" exact component={LandingPage} />
-        <PrivateRoute path="/users" component={UsersPage} />
+        <div className={cn('page-wrapper', { 'toggled': navToggle })}>
+          <Navigation setNavToggle={setNavToggle}></Navigation>
+          <main className="page-content">
+            <Route path="/" exact component={LandingPage} />
+            <PrivateRoute path="/users" component={UsersPage} />
+          </main>
+        </div>
       </Store.Context>
     </Router>
   )
--- client/src/components/navigation.tsx
+++ client/src/components/navigation.tsx
@@ -1,27 +1,56 @@
-import React, { useContext, FC }  from 'react'
+import React, { useContext, FC, Fragment, SetStateAction, Dispatch, ComponentType }  from 'react'
 import { Link } from "react-router-dom"
 
 import * as Store from '../context/store'
+import '../stylesheets/navigation.css'
 
-const Navigation: FC = () => {
+interface IProps {
+  setNavToggle: Dispatch<SetStateAction<boolean>>,
+}
+
+const Navigation: FC<IProps> = (props) => {
   const { currentUser } = useContext(Store.AuthCotext)
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-        {currentUser && (
-          <li>
-            <a href="/auth/logout">Logout {currentUser.username}</a>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <div className="chiller-theme">
+      <a id="show-sidebar" className="btn btn-sm btn-dark" onClick={() => props.setNavToggle(true)} href="javascript:void(0);">
+        <i className="fas fa-bars"></i>
+      </a>
+      <nav id="sidebar" className="sidebar-wrapper">
+        <div className="sidebar-content">
+          <div className="sidebar-brand">
+            <Link to="/">yThrift</Link>
+            <div id="close-sidebar" onClick={() => props.setNavToggle(false)}>
+              <i className="fas fa-times"></i>
+            </div>
+          </div>
+          <div className="sidebar-menu">
+            <ul>
+              <li>
+                <Link to="/">
+                  <i className="fa fa-home"></i>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/users">
+                  <i className="fa fa-users"></i>
+                  Users
+                </Link>
+              </li>
+              {currentUser && (
+                <li>
+                  <a href="/auth/logout">
+                    <i className="fa fa-sign-out-alt"></i>
+                    Logout {currentUser.username}
+                  </a>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   )
 }
 
